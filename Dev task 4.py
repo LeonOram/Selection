a75d5d4e43f6d2a170117ab4cdef2c4a121fc08b
Leon Oram <45027@longroad.ac.uk>
2014-09-30 12:58:48 +0100

    comp

---
+++ Dev task 4.py
@@ -0,0 +1,20 @@
+#Leon Oram
+#30-09-2014
+#selection dev task 4
+
+score = int(input("Please enter ther given score: "))
+
+if score <= 40:
+    print("U")
+elif score <= 50:
+    print("E")
+elif score <= 60:
+    print("D")
+elif score <= 70:
+    print("C")
+elif score <= 80:
+    print("B")
+elif score <= 100:
+    print("A")
+else:
+    print("Error")
